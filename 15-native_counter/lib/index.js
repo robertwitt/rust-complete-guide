3801543f9041b3948a42f41a614648f13758bb0d
robertwitt <robert_witt@icloud.com>
2022-04-01 14:18:21 +0200

    Exercise 15: Native counter

---
+++ 15-native_counter/lib/index.js
@@ -0,0 +1,2 @@
+var word_counter = require("../native");
+module.exports = word_counter.count_words;
---
+++ 15-native_counter/main.js
@@ -0,0 +1,3 @@
+var count_words_func = require(".");
+var wc = count_words_func("A test text to test native module", "test");
+console.log(wc);
